a945174646174b8732853e7059f84aa2ab9dd0c0
Shashank Billagiri <33669421+shashankBillagiri@users.noreply.github.com>
2023-05-24 09:51:56 -0500

    Understanding Terrafrom  Concepts (#14)
    * First Commit
    
    rebasing
    
    * Updated Notes
    
    * locals Practise
    
    * Understanding Locals

---
+++ locals.tf
@@ -0,0 +1,14 @@
+locals {
+  shell = ["crunchy", "soft"]
+  tags = {
+    number = 42
+    item   = "towel"
+    planet = "Earth"
+    poetry = "Vogon"
+  }
+  meaning_of_life = "What is 6 x 7? ${local.tags["number"]}"
+
+   # using a variable in Local
+
+  ApplicationName = "${var.app_name}"
+}
